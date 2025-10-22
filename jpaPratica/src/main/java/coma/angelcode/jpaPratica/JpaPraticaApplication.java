10f41ddea622d085b2f28ef995d0b59149e2b13d
Mael7w7 <ange_lito2002@hotmail.com>
2025-10-22 00:17:54 -0500

    hola

---
+++ jpaPratica/src/main/java/coma/angelcode/jpaPratica/JpaPraticaApplication.java
@@ -0,0 +1,13 @@
+package coma.angelcode.jpaPratica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaPraticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaPraticaApplication.class, args);
+	}
+
+}
---
+++ jpaPratica/src/test/java/coma/angelcode/jpaPratica/JpaPraticaApplicationTests.java
@@ -0,0 +1,13 @@
+package coma.angelcode.jpaPratica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaPraticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
